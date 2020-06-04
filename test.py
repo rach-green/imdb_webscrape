4367e38e1565181558570c9600896883ab69ad2f
Rachel Green <reg246@cornell.edu>
2020-06-04 10:55:17 -0400

    fixed warning about lxml

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 
 url = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating'
 resp = requests.get(url)
-soup = BeautifulSoup(resp.text)
+soup = BeautifulSoup(resp.text, "lxml")
 
 #getting titles of all top_250
 llist = soup.find_all('h3',{'class':'list-item-header'})
