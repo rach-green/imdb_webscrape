5ba919df9987ece70e0f0b00747dbb1c1b434c74
Rachel Green <reg246@cornell.edu>
2020-06-04 10:51:13 -0400

    getting titles of all top 250

--- test.py
+++ test.py
@@ -3,3 +3,10 @@ import requests
 
 url = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating'
 resp = requests.get(url)
+soup = BeautifulSoup(resp.text)
+
+#getting titles of all top_250
+llist = soup.find_all(‘h3’,{‘class’:‘list-item-header’})
+for x in llist:
+    for y in x.find_all(‘a’):
+        print(y.text)
