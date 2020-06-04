f9d07b2780b6d98bf5a6bbf936057e66282dc341
Sophie Keller <you@example.com>
2020-06-04 10:58:36 -0500

    added more scraping

--- test.py
+++ test.py
@@ -9,4 +9,28 @@ soup = BeautifulSoup(resp.text, features = "lxml")
 llist = soup.find_all('h3',{'class':"lister-item-header"})
 for x in llist:
     for y in x.find_all('a'):
-        print(y.text)
+        titles = y.text
+        print(titles)
+    for y in x.find_all('span',{'class':'lister-item-year'}):
+        years = y.text
+        print(years)
+    for y in x.find_all('span',{'class':'lister-item-index'}):
+        ranking = y.text
+        print(ranking)
+
+
+content = soup.find_all('p', {'class':'text-muted'})
+for x in content:
+    for y in x.find_all('span',{'class':'certificate'}):
+        ratings = y.text
+        print(ratings)
+
+people = soup.find_all('p',{'class':""})
+for x in people:
+    for y in x.find_all('a'): #think about adding span for director/actor
+            people = y.text
+            print(people)
+
+metascores = soup.find_all('span',{'class':'metascore'})
+for x in metascores:
+    print(x.text)
