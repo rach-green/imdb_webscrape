6f7234441549959c0ee08e4156b1cd14bf8e2242
Sophie Keller <you@example.com>
2020-06-10 10:46:53 -0500

    experimenting with dbconnection

--- dbconnection.js
+++ dbconnection.js
@@ -1,14 +1,23 @@
 //[code language = "javascript"]
 var mysql = require('mysql');
 
-var connection = mysql.createPool({
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    passsword: 'dalehollow',
-    database: 'mydatabase'
+    passsword: 'dalehollow'
+    // database: 'mydatabase'
 });
 
-connection.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!");
-});
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+ });
+
+// connection.connect(function(err){
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
+//module.exports = connection;
