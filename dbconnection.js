136086753902fd98abab3fba4fbd86f1d4b75365
Sophie Keller <you@example.com>
2020-06-10 11:21:49 -0500

    tried to query

--- dbconnection.js
+++ dbconnection.js
@@ -4,20 +4,21 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'dalehollow'
-    // database: 'mydatabase'
+    password: 'dalehollow',
+    database: 'mydatabase'
 });
-
+//worked
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+//  });
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
- });
-
-// connection.connect(function(err){
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-//module.exports = connection;
+  if (err) throw err;
+  connection.query("SELECT * FROM movies WHERE id = '1'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
