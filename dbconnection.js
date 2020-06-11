de57fabcd845d4072de7fe5d237e0bcaa34de539
Rachel Green <reg246@cornell.edu>
2020-06-11 15:00:47 -0400

    added more example querues

--- dbconnection.js
+++ dbconnection.js
@@ -17,12 +17,25 @@ var connection = mysql.createConnection({
 //  });
 connection.connect(function(err) {
   if (err) throw err;
-  connection.query("SELECT * FROM movies WHERE id = '1'", function (err, result) {
+  // connection.query("SELECT * FROM movies WHERE id = '1'", function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  // connection.query("SELECT summary FROM movies WHERE summary LIKE '%violence%'", function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  // connection.query("SELECT * FROM movies WHERE year BETWEEN 2016 AND 2020", function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  // connection.query("SELECT * FROM movies WHERE rating = 'R' AND year < 1990", function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  connection.query("SELECT * FROM movies WHERE rating = 'PG' AND summary LIKE '%spirits%'", function (err, result) {
     if (err) throw err;
     console.log(result);
   });
-  connection.query("SELECT summary FROM movies WHERE summary LIKE '%violence%'", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
+  connection.end();
 });
