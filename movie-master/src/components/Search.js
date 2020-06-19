37b1178e21d38cad0e79b9f9dcecde9953d75184
Sophie Keller <you@example.com>
2020-06-19 14:59:26 -0500

    worked on search field

---
+++ movie-master/src/components/Search.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import ReactSearchBox from 'react-search-box'
+
+export default class Search extends Component {
+    data = [
+    {
+      key: 'john',
+      value: 'John Doe',
+    },
+    {
+      key: 'jane',
+      value: 'Jane Doe',
+    },
+    {
+      key: 'mary',
+      value: 'Mary Phillips',
+    },
+    {
+      key: 'robert',
+      value: 'Robert',
+    },
+    {
+      key: 'karius',
+      value: 'Karius',
+    },
+  ]
+  fields = []
+
+  addField = name => {
+    this.fields.push(name);
+    console.log(this.fields);
+  };
+
+  render() {
+    return (
+      <div className = "search-container">
+        <ReactSearchBox
+        placeholder="Search a Person"
+        value="Doe"
+        data={this.data}
+        onSelect={record => this.addField(record)}
+        />
+        <div>{this.fields.toString()}</div>
+
+      </div>
+  );
+  }
+}
--- movie-master/src/screens/Analytics.js
+++ movie-master/src/screens/Analytics.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Slider2 from '../components/Slider2';
+// import Slider2 from '../components/Slider2';
 import Range from '../components/Range';
+import Search from '../components/Search';
 
 
 //export default says return this render function. import this class when you import this file no matter what.
@@ -10,8 +11,9 @@ export default class Analytics extends React.Component{//react has a component c
             <div className = "analytics-container">
                 <div className = "range-div">
                     <div className = "analytics-title">years</div>
-                    <Range />
+                    // <Range />
                 </div>
+                <Search />
             </div>
         );
     }
