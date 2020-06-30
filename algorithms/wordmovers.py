c3bfe18e0b9cd4abe0e48555258427e14e60abf5
Sophie Keller <you@example.com>
2020-06-30 10:09:54 -0500

    worked on algorithms

---
+++ algorithms/wordmovers.py
@@ -0,0 +1,21 @@
+import gensim.downloader as api
+from nltk.corpus import stopwords
+from nltk import download
+
+
+model = api.load('word2vec-google-news-300')
+download('stopwords')  # Download stopwords list.
+stop_words = stopwords.words('english')
+
+def preprocess(sentence):
+    return [w for w in sentence.lower().split() if w not in stop_words]
+
+first = "Two imprisoned men bond over a number of years finding solace and eventual redemption through acts of common decency"
+second = "In Gotham City mentally troubled comedian Arthur Fleck is disregarded and mistreated by society He then embarks on a downward spiral of revolution and bloody crime This path brings him face-to-face with his alter-ego the Joker"
+
+first = preprocess(first)
+second = preprocess(second)
+
+distance = model.wmdistance(sentence_obama, sentence_president)
+print('distance = %.4f' % distance)
+model = api.load('word2vec-google-news-300')
