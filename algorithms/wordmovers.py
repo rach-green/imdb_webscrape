acea1f0761b32add66161f69756d495a1842f4ef
Rachel Green <reg246@cornell.edu>
2020-07-01 10:40:22 -0400

    made some changes in wordmovers

--- algorithms/wordmovers.py
+++ algorithms/wordmovers.py
@@ -4,19 +4,18 @@ from nltk.corpus import stopwords
 from nltk import download
 
 
-model = api.load('word2vec-google-news-300')
+model = api.load('word2vec-google-news-300') #takes a long time!
 download('stopwords')  # Download stopwords list.
 stop_words = stopwords.words('english')
 
 def preprocess(sentence):
     return [w for w in sentence.lower().split() if w not in stop_words]
 
-first = "Two imprisoned men bond over a number of years finding solace and eventual redemption through acts of common decency"
-second = "In Gotham City mentally troubled comedian Arthur Fleck is disregarded and mistreated by society He then embarks on a downward spiral of revolution and bloody crime This path brings him face-to-face with his alter-ego the Joker"
+first = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency"
+second = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son"
 
 first = preprocess(first)
 second = preprocess(second)
 
 distance = model.wmdistance(first, second)
 print('distance = %.4f' % distance)
-model = api.load('word2vec-google-news-300')
