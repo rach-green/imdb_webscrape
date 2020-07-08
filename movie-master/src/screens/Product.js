2fad08d45cd02a338c8fc6978344e8120476ae76
Sophie Keller <you@example.com>
2020-07-08 11:20:43 -0400

    Configuration to deploy to heroku

--- movie-master/src/screens/Product.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-//export default says return this render function. import this class when you import this file no matter what.
-export default class Product extends React.Component{//react has a component class
-    render(){
-        return((<div>product</div>));
-    }
-}
